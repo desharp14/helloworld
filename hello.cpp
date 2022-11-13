80c5944bfd3a0b79faccfd1e61bb2bc32b55d174
desharp14 <113667264+desharp14@users.noreply.github.com>
2022-11-13 18:32:25 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//header file library
+#include <iostream>
+//use names for objects and variables from the standard library
+using namespace std;
+
+//main function
+int main() {
+  //output Hello World!; endl to insert a new line
+  cout << "Hello World!" << endl;
+  //end main function
+  return 0;
+}
